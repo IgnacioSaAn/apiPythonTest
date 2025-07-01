b5283036ea5a0a3164be9c483e747558b8ed5ee0
IgnacioSaAn <ignaciosanchezagnello@gmail.com>
2025-07-01 11:19:09 -0300

    cambio conexion db

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost/penTest"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://test_7o5s_user:GhvUIdDlPrcFDO8T4aYFu24lur5dxWtb@dpg-d1hupd3uibrs73805i5g-a.oregon-postgres.render.com/test_7o5s"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
